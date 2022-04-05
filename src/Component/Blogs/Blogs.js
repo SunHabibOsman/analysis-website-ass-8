f7e24d065c2f8315ece55cefe4ea3be24b7130be
rajia124 <rajia5203@gmail.com>
2022-04-05 23:02:42 +0600

    blog added

--- src/Component/Blogs/Blogs.js
+++ src/Component/Blogs/Blogs.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './Blogs.css'
 
 const Blogs = () => {
   return (
-    <div>
-      <h1>blog</h1>
+    <div className='container'>
+      <div className='d-flex '>
+        <div>
+          <h1 className='fw-bold text-start mt-2 px-3'>What is Context Api</h1>
+          <h4 className='text-start mt-2 px-3'>The React Context API was been around as an experimental feature , solving one major problem React problem — prop drilling. So its make easy to get prop.</h4>
+        </div>
+        <img className='image-context' src="context.png" alt="" />
+      </div>
+      <div className='d-flex'>
+        <div><h1 className='fw-bold text-start mt-2 px-3'>What is Semantic Tag</h1>
+          <h4 className='text-start mt-2 px-3'>Semantic HTML5 addresses this shortcoming by defining specific tags to indicate clearly what role is played by the content those tags contain. That explicit information helps robots/crawlers like Google and Bing to better understand which content is important, which is a subsidiary, which is for navigation, and so on.</h4></div>
+        <img className='image-context' src="semantic.jpg" alt="" />
+      </div>
     </div>
   );
 };
